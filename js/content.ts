e898a63081489e96f08ab7f9a9b83c6006eb1423
filipnaumovic <filip@salsitasoft.com>
2014-04-16 14:02:28 +0200

    changed font to Lato, removed comment

--- js/content.ts
+++ js/content.ts
@@ -9,7 +9,6 @@ var _jQuery = $.noConflict(true);
 (function ($) {
 
     var pendingRequest = false;
-    var div = document.createElement('div');
 
     var embedlyRequest = function(callback) {
         if (pendingRequest) {
@@ -67,7 +66,7 @@ var _jQuery = $.noConflict(true);
 
     var displayFrame = function (text): HTMLElement {
 
-        //var div = document.createElement('div');
+        var div = document.createElement('div');
         div.style.backgroundColor = 'white';
         div.style.position = "absolute";
         div.style.top = "0px";
